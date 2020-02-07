6aba9673edabf73faab501c26a8b248494ccca30
Unknown <60795379+sirunknown009@users.noreply.github.com>
2020-02-07 23:22:01 +0330

    Add files via upload

---
+++ post.php
@@ -0,0 +1,8 @@
+//code by Bandit
+//location v2
+<?php
+$json = (file_get_contents('php://input'));
+$dejson = json_decode($json, true);
+$location = $dejson['data'];
+shell_exec("python3 send.py ".$location);
+?>
